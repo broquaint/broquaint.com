e530f2312f8f9029b50da236d977235d7209be85
Dan Brook <broq@cpan.org>
2013-03-25 12:25:00 +0100

    Route through resources/public.
    Instead of relying on a symlink because I wasn't sure of what I was
    doing before. Also use file-response for 404s.

--- src/broquaint/handler.clj
+++ src/broquaint/handler.clj
@@ -1,19 +1,16 @@
 (ns broquaint.handler
-  (:use compojure.core
+  (:use compojure.core [ring.util.response :only (file-response)]
         ring.adapter.jetty)
   (:require [compojure.handler :as handler]
-            [compojure.route   :as route]
-            )
+            [compojure.route   :as route])
   (:gen-class))
 
 (defroutes app-routes
-  (route/files "/")
-  (route/not-found "Err dunno!\n"));(resource-file-str "../404.html")
+  (route/files "/" {:root "resources/public"})
+  (route/not-found (file-response "resources/404.html")))
 
 (def app
-  (->
-   (handler/site app-routes)
-   ))
+  (handler/site app-routes))
 
 (defn -main [& args]
   (run-jetty app {:port 3080 :join? false}))
