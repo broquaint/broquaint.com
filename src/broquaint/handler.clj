99cbc6e10580475eb4fdc25fb30ac44e700b0256
Dan Brook <dan@broquaint.com>
2014-01-04 12:32:53 +0100

    Fix 404 response.
    So we respond with a page instead of a wall of HTML.

--- src/broquaint/handler.clj
+++ src/broquaint/handler.clj
@@ -1,13 +1,17 @@
 (ns broquaint.handler
-  (:use compojure.core [ring.util.response :only (file-response)]
+  (:use compojure.core [ring.util.response :only (file-response content-type)]
         ring.adapter.jetty)
   (:require [compojure.handler :as handler]
             [compojure.route   :as route])
   (:gen-class))
 
 (defroutes app-routes
+  ;; Landing (and currently only) page.
   (route/files "/" {:root "resources/public"})
-  (route/not-found (file-response "resources/404.html")))
+  ;; Nice h5bp 404 page.
+  (route/not-found 
+   (-> (file-response "resources/404.html")
+       (content-type "text/html"))))
 
 (def app
   (handler/site app-routes))
