77d5a7c17fe311272b7fd772f8ce5bf34a2ccae4
Dan Brook <dan@broquaint.com>
2024-02-19 15:23:14 +0700

    Update project dependencies
    They were all embarrassingly old. Also politely deny AI scrapers.

--- project.clj
+++ project.clj
@@ -1,16 +1,17 @@
 (defproject broquaint "0.1.4"
   :description "The broquaint.com website"
   :url "http://broquaint.com/"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [ring/ring-jetty-adapter "1.1.8"]
-                 [compojure "1.1.5"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [ring/ring-core "1.11.0"]
+                 [ring/ring-jetty-adapter "1.11.0"]
+                 [compojure "1.7.1"]
                  ;; broquaint.github-repos deps vv
-                 [ring/ring-json "0.1.2"]
-                 [clj-http "0.7.8"] ;; Needed otherwise tentacles blows up
-                 [irresponsible/tentacles "0.6.6"]
-                 [org.clojure/core.cache "0.6.3"]]
-  :plugins [[lein-ring "0.8.2"]]
+                 [ring/ring-json "0.5.1"]
+                 [irresponsible/tentacles "0.6.9" :exclusions [cheshire]]
+                 [org.clojure/java.classpath "1.0.0"]
+                 [org.clojure/core.cache "1.0.225"]]
+  :plugins [[lein-ring "0.12.6"]]
   :ring {:handler broquaint.handler/app :port 3080}
   :profiles
-  {:dev {:dependencies [[ring-mock "0.1.3"]]}}
+  {:dev {:dependencies [[ring/ring-mock "0.4.0"]]}}
   :main broquaint.handler)
