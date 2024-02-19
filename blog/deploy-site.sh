b4d1e1367ee524a2633dc9dd65990eacd6e77a5b
Dan Brook <dan@broquaint.com>
2024-02-19 15:25:28 +0700

    Add jekyll blog
    It's all a bit smoke and mirrors but this works for now.

---
+++ blog/deploy-site.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# A bit basic but it'll do!
+echo '>>> Building site ...'
+if jekyll build
+then
+    echo '>>> Syncing files ...'
+    rsync -av _site/index.html _site/blog _site/assets miro:dev/broquaint/resources/public/
+fi
